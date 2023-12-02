26039c6ff3f00524901fa878c95423eb97a60191
Sanket Garde <sanketgarde7473@gmail.com>
2023-12-02 14:36:28 +0530

    Added: Java functionality for mood Analyser updated with exception updated

--- src/main/java/com/bridgelabz/mood_analyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/mood_analyser/MoodAnalyser.java
@@ -4,15 +4,47 @@ package com.bridgelabz.mood_analyser;
 *@Desc: MoodAnalyser is a class which is to check the mood analyser.
 *@param is a containing String message.
  **/
-public class MoodAnalyser {
-    /*
-    *@Desc : analyseMood is a method checking a SAD and HAPPY Message.
-    * @param is a containing the Message variable.
-     **/
+public class MoodAnalyser  {
+    String message;
+    public MoodAnalyser() {
+
+    }
+//constructor is initializing a variable
+    public MoodAnalyser(String message) {
+        this.message=message;
+    }
+/*@Desc: analyseMood() return the sad or happy mood.
+    @return :returning a sad or happy.
+
+ **/
     public String analyseMood(String message) {
         if (message.contains("I am in sad mood")){
             return "SAD";
         } else if (message.contains("I am in happy mood")) {
+            return "HAPPY";
+        }
+        return null;
+    }
+
+    /*@Desc:analyseMoodConstructor() it is contains a sad or happy mood.
+     @return :returning a sad or happy
+     **/
+    public String analyseMoodConstructor() {
+        if (message.contains("I am in sad mood")){
+            return "SAD";
+        } else if (message.contains("I am in happy mood")) {
+            return "HAPPY";
+        }
+        return null;
+    }
+    //analyseMoodConstructorUsingTryCatch () function is to handle a null pointer exception
+    public String analyseMoodConstructorUsingTryCatch() {
+        try {
+            if (message.contains(null)){
+                throw new NullPointerException();
+            }
+        }catch (NullPointerException e){
+            System.out.println(e.getLocalizedMessage());
             return "HAPPY";
         }
         return null;
--- src/test/java/com/bridgelabz/mood_analyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/mood_analyser/MoodAnalyserTest.java
@@ -10,17 +10,35 @@ import org.junit.jupiter.api.Test;
 **/
 public class MoodAnalyserTest {
     MoodAnalyser moodAnalyser=new MoodAnalyser();
-    //givenTheMoodShouldReturnSad() is analyseMood() comparing with the sad mood.
     @Test
+    //givenTheMoodShouldReturnSad() is analyseMood() comparing with the sad mood.
     public void givenTheMoodShouldReturnSad(){
         String mood=moodAnalyser.analyseMood("I am in sad mood");
         Assertions.assertEquals("SAD",mood);
     }
-
-    //givenTheMoodShouldReturnHappy() is analyseMood() comparing with the happy mood.
     @Test
+    //givenTheMoodShouldReturnHappy() is analyseMood() comparing with the sad mood.
     public void givenTheMoodShouldReturnHappy(){
         String mood=moodAnalyser.analyseMood("I am in happy mood");
         Assertions.assertEquals("HAPPY",mood);
     }
+    @Test
+    //givenTheMoodShouldReturnSadUsingConstructor () comparing with the sad mood.
+    public void givenTheMoodShouldReturnSadUsingConstructor(){
+        MoodAnalyser moodAnalyser=new MoodAnalyser("I am in sad mood");
+        Assertions.assertEquals("SAD",moodAnalyser.analyseMoodConstructor());
+    }
+    @Test
+    //givenTheMoodShouldReturnSadUsingConstructor () comparing with the happy  mood.
+    public void givenTheMoodShouldReturnHappyUsingConstructor(){
+        MoodAnalyser moodAnalyser=new MoodAnalyser("I am in happy mood");
+        Assertions.assertEquals("HAPPY",moodAnalyser.analyseMoodConstructor());
+    }
+    @Test
+    //givenTheNullMoodShouldReturnHappyHandleUsingTryCatch () for null pointer exception
+    public void givenTheNullMoodShouldReturnHappyHandleUsingTryCatch(){
+        MoodAnalyser moodAnalyser=new MoodAnalyser(null);
+        Assertions.assertEquals("HAPPY",moodAnalyser.analyseMoodConstructorUsingTryCatch());
+    }
+
 }
